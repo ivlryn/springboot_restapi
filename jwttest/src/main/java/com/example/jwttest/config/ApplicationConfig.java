11bb0ffcb6254944215e47e6147f92f590850778
ivlryn <neththeng264@gmail.com>
2026-03-13 22:30:19 +0700

    config

--- jwttest/src/main/java/com/example/jwttest/config/ApplicationConfig.java
+++ jwttest/src/main/java/com/example/jwttest/config/ApplicationConfig.java
@@ -1,14 +1,36 @@
 package com.example.jwttest.config;
 
+import com.example.jwttest.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepo userRepo;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        return provider;
     }
 }
